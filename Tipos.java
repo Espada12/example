ea8ec7ea34d57792a50b0a535adc86fbf9d9f20a
Espada12 <tapsumi13@gmail.com>
2023-03-14 20:26:07 -0300

    se agregra el proyecto

---
+++ Tipos.java
@@ -0,0 +1,41 @@
+package com.example;
+
+public class Tipos {
+    public static void main(String[] args) {
+
+        //1.Numericos
+
+        //1.1 Enteros
+        byte var1 = 5;
+        short var2 = 10;
+        int var3 = 30;
+        long var4 = 100;
+
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var3);
+        System.out.println(var4);
+
+        //1.2 Decimales
+
+        float var5 = 1.4f;
+        double var6 = 11.2d;
+
+        System.out.println(var5);
+        System.out.println(var6);
+
+        //2. boolean
+        boolean var7 = false;
+        boolean var8 = true;
+
+        System.out.println(var7);
+        System.out.println(var8);
+
+        //3. texto
+        String var9 = "fff";
+        char var10 = 'a' ;
+
+        System.out.println(var9);
+        System.out.println(var10);
+    }
+}
